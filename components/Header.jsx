a0f823a047c4de26572409315e441aec7975265e
satyamjha2001 <satyamjha23112001@gmail.com>
2024-12-25 01:57:45 +0530

    commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className={`header-container ${isDark ? "dark" : ""}`}>
       <div className="header-content">
         <h2 className="title">
-          <a href="/">Where in the world?</a>
+          <a href="/">Explore the World</a>
         </h2>
         <p
           className="theme-changer"
